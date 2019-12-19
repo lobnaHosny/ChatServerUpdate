2c5b47261337a6b31aa334beddb92e50eb360bda
lobnaHosny <45228631+lobnaHosny@users.noreply.github.com>
2019-12-19 16:00:38 +0800

    Add thread.sleep()

--- src/main/java/ChatServer/src/Server/Server.java
+++ src/main/java/ChatServer/src/Server/Server.java
@@ -36,6 +36,7 @@ public class Server {
 						DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
 						c = new Connection(socket,this,dis,dos );
 						Thread t = new Thread(c);
+						Thread.sleep(500);
 
 						t.start();
 						list.add(c);
