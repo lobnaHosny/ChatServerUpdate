584f5f44a0921c706406e44de4e97cb421db5b28
lobnaHosny <45228631+lobnaHosny@users.noreply.github.com>
2019-12-22 15:03:16 +0800

    Updated client more modularity

--- src/main/java/ChatServer/src/Client/Client.java
+++ src/main/java/ChatServer/src/Client/Client.java
@@ -9,38 +9,48 @@ public class Client
     private static DataOutputStream dos;
     private static DataInputStream dis;
     final static int ServerPort = 8080;
+    Socket s = null;
+    String finalmessage;
 
-    public static void main(String args[])
+    public Client(int ServerPort)
     {
         Scanner scn = new Scanner(System.in);
 
         // getting localhost ip
         try {
-            InetAddress ip = InetAddress.getByName("localhost");
+            InetAddress ip = InetAddress.getByName("127.0.0.1");
 
 
-        // establish the connection
-        Socket s = new Socket("127.0.0.1", ServerPort);
+            // establish the connection
+            s = new Socket(ip, ServerPort);
+
+            // obtaining input and out streams
+            dis = new DataInputStream(s.getInputStream());
+            dos = new DataOutputStream(s.getOutputStream());
+            if(dis != null){
+                readMessage();
+                sendMessage(scn);
+            }
+
+        } catch (Exception e) {
+            System.err.println("Invalid port");
+            //e.printStackTrace();
+        }
 
 
-        // obtaining input and out streams
-        dis = new DataInputStream(s.getInputStream());
-        dos = new DataOutputStream(s.getOutputStream());
+    }
 
-
+    public void sendMessage(Scanner scn){
         // sendMessage thread
         Thread sendMessage = new Thread(new Runnable()
         {
             @Override
             public void run() {
-                while (true) {
+                while (scn.hasNext()) {
 
                     // read the message to deliver.
                     String msg = scn.nextLine();
-
-
                     try {
-
                         // write on the output stream
                         dos.writeUTF(msg);
                     } catch (IOException e) {
@@ -49,7 +59,9 @@ public class Client
                 }
             }
         });
-
+        sendMessage.start();
+    }
+    public String readMessage(){
         Thread readMessage = new Thread(new Runnable()
         {
             @Override
@@ -58,32 +70,36 @@ public class Client
                 while (true) {
                     try {
                         // read the message sent to this client
-                        if(dis.available()>0){
+                        if(dis.available()>0 && dis!=null){
                             String msg = dis.readUTF();
-                            System.out.println(msg);
+                            finalmessage = msg;
+                            if(!msg.equals("Update List")){
+                                System.out.println(msg);
+                            }
                             if(msg.equals("QUIT")){
                                 s.close();
                                 dis.close();
-                                break;
+                                dos.close();
+                                System.exit(0);
+                                //break;
                             }
                         }
 
                     } catch (IOException e) {
 
+                        //System.err.println(e);
                         e.printStackTrace();
                     }
                 }
             }
         });
-       readMessage.start();
-       sendMessage.start();
+        readMessage.start();
+        return finalmessage;
+    }
 
 
-        } catch (Exception e) {
-            System.err.println("wrong port");
-            //e.printStackTrace();
-        }
-
-
+    public static void main(String args[])
+    {
+        Client client = new Client(ServerPort);
     }
 }
